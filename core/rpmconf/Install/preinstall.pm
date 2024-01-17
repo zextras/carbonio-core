10921a58a4dd2d9a8c2dd53eb84141e0fd6baa3e
Davide Frison <davide.frison@zextras.com>
2024-01-17 18:17:48 +0100

    refactor: move carbonio-build rpmconf files
    - some files definition are in carbonio-build

---
+++ core/rpmconf/Install/preinstall.pm
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+# 
+# SPDX-FileCopyrightText: 2022 Synacor, Inc.
+# SPDX-FileCopyrightText: 2022 Zextras <https://www.zextras.com>
+#
+# SPDX-License-Identifier: GPL-2.0-only
+# 
+
+package preinstall;
+
+sub mainMenuExtensions {
+	my ($mm, $i) = (@_);
+	return $i;
+}
+
+1
