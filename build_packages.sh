50d1154575c9696bfa4a66bb2c74a5896c852e2d
Davide Frison <36330277+frisonisland@users.noreply.github.com>
2024-07-04 16:58:59 +0200

    refactor: [CO-1046] embedded jetty (#67)
    * refactor: remove common-core-jar and common-core-libs in favor of mailbox-jar
    
    * chore: core-utils devel

--- build_packages.sh
+++ build_packages.sh
@@ -7,10 +7,10 @@
 
 TARGET=$1
 
-if [[ ${TARGET} == '' || ${TARGET} == 'ubuntu-focal' ]]; then
-  docker run --entrypoint=yap -v "$(pwd)/artifacts:/artifacts" -v "$(pwd)":/tmp/staging registry.dev.zextras.com/jenkins/pacur/ubuntu-20.04:v2 build ubuntu-focal -c /tmp/staging -s
+if [[ ${TARGET} == '' || ${TARGET} == 'ubuntu-jammy' ]]; then
+  docker run --entrypoint=yap -v "$(pwd)/artifacts/ubuntu-jammy:/artifacts" -v "$(pwd)":/tmp/staging registry.dev.zextras.com/jenkins/pacur/ubuntu-22.04:v2 build ubuntu-jammy -c /tmp/staging -s
 else
   if [[ ${TARGET} == 'rocky-8' ]]; then
-    docker run --entrypoint=yap -v "$(pwd)/artifacts:/artifacts" -v "$(pwd)":/tmp/staging registry.dev.zextras.com/jenkins/pacur/rocky-8:v2 build rocky-8 -c /tmp/staging -s
+    docker run --entrypoint=yap -v "$(pwd)/artifacts/rocky-8:/artifacts" -v "$(pwd)":/tmp/staging registry.dev.zextras.com/jenkins/pacur/rocky-8:v2 build rocky-8 -c /tmp/staging -s
   fi
 fi
